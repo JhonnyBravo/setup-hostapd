dba7d14a2cf77b1e98633634d7ace3dbbee58ecc
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-04-06 16:22:35 +0900

    renamed: scripts/{provision_hostapd.sh --> provision_ap.sh}
    アクセスポイントの構成に使用するパッケージのリストを変更。
    ヘルプドキュメントの仕様を変更。

---
+++ scripts/provision_ap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+package="hostapd iw dnsmasq haveged"
+
+i_flag=0
+u_flag=0
+h_flag=0
+
+i_description="-i ${package} をインストールします。"
+u_description="-u ${package} をアンインストールします。"
+h_description="-h ヘルプを表示します。"
+
+function get_usage(){
+  cat <<_EOT_
+Usage:
+  ${0} [-i] [-u] [-h]
+
+Description:
+  ${package} をインストール / アンインストールします。
+
+Options:
+  ${i_description}
+  ${u_description}
+  ${h_description}
+_EOT_
+
+exit 1
+}
+
+while getopts "iuh" option
+do
+  case $option in
+    i)
+      i_flag=1
+      ;;
+    u)
+      u_flag=1
+      ;;
+    h)
+      h_flag=1
+      ;;
+    \?)
+      h_flag=1
+      ;;
+  esac
+done
+
+if [ $i_flag -eq 1 ]; then
+  # shellcheck disable=SC2086
+  apt-get install $package
+elif [ $u_flag -eq 1 ]; then
+  # shellcheck disable=SC2086
+  apt-get purge $package
+elif [ $h_flag -eq 1 ]; then
+  get_usage
+fi
