54807302775a6de8d67db4967913980d019a5d84
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-04-06 17:19:44 +0900

    new file: scripts/get_ap_template.sh
    scripts/{get_conf_template.sh, get_init_template.sh} を統合。

---
+++ scripts/get_ap_template.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+template_directory="template"
+
+h_flag=0
+h_description="-h ヘルプを表示します。"
+
+function get_usage(){
+  cat <<_EOT_
+Usage:
+  ${0} [-h]
+
+Description:
+  アクセスポイントの設定に使用するファイルを
+  ./${template_directory} へ一括コピーします。
+
+Options:
+  ${h_description}
+_EOT_
+
+exit 1
+}
+
+while getopts "h" option
+do
+  case $option in
+    h)
+      h_flag=1
+      ;;
+    \?)
+      h_flag=1
+      ;;
+  esac
+done
+
+if [ $h_flag -eq 1 ]; then
+  get_usage
+fi
+
+if [ ! -d "$template_directory" ]; then
+  mkdir "$template_directory"
+fi
+
+provision_dnsmasq_conf.sh -g "$template_directory"
+provision_hostapd_config.sh -g "$template_directory"
+provision_hostapd_init.sh -g "$template_directory"
+provision_interfaces.sh -g "$template_directory"
+provision_network_manager.sh -g "$template_directory"
+provision_rc_local.sh -g "$template_directory"
+provision_sysctl_conf.sh -g "$template_directory"
