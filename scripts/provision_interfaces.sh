264c15cd21367f5b201965abe562841209ba220f
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-04-06 16:29:06 +0900

    new file: scripts/provision_interfaces.sh

---
+++ scripts/provision_interfaces.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+default_path="/etc/network/interfaces"
+template_path="/usr/share/doc/ifupdown/examples/network-interfaces.gz"
+file_name=$(basename ${default_path})
+
+g_flag=0
+t_flag=0
+s_flag=0
+h_flag=0
+
+g_description=$(
+  cat <<_EOT_
+  -g directory_path   ${default_path} を
+                      directory_path へコピーします。
+_EOT_
+)
+
+t_description=$(
+  cat <<_EOT_
+  -t directory_path   ${template_path} を
+                      directory_path へコピーします。
+_EOT_
+)
+
+s_description=$(
+  cat <<_EOT_
+  -s template_path   template_path を
+                     ${default_path} へインストールします。
+_EOT_
+)
+
+h_description="  -h ヘルプを表示します。"
+
+function get_usage(){
+  cat <<_EOT_
+Usage:
+  ${0} [-g] [-t] directory_path
+  ${0} -s template_path
+  ${0} -h
+
+Description:
+  ${default_path} をコピーまたはインストールします。
+
+Options:
+${g_description}
+${t_description}
+${s_description}
+${h_description}
+_EOT_
+
+exit 1
+}
+
+while getopts "g:t:s:h" option
+do
+  case $option in
+    g)
+      g_flag=1
+      ;;
+    t)
+      t_flag=1
+      ;;
+    s)
+      s_flag=1
+      ;;
+    h)
+      h_flag=1
+      ;;
+    \?)
+      h_flag=1
+      ;;
+  esac
+done
+
+shift $((OPTIND - 2))
+
+if [ $g_flag -eq 1 ]; then
+  directory_name="$1"
+  cat "$default_path" > "${directory_name}/${file_name}"
+elif [ $t_flag -eq 1 ]; then
+  directory_name="$1"
+  zcat "$template_path" > "${directory_name}/${file_name}"
+elif [ $s_flag -eq 1 ]; then
+  source_path="$1"
+  install "$source_path" "$default_path" -m 0644
+elif [ $h_flag -eq 1 ]; then
+  get_usage
+fi
