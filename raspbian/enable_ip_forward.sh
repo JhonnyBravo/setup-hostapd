9b6873564da397d9eda1c38161dc6398c1766ed2
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-11-13 03:15:33 +0900

    modified: raspbian/enable_ip_forward.sh
    変数名を変更。

--- raspbian/enable_ip_forward.sh
+++ raspbian/enable_ip_forward.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gateway_ip='192.168.1.1'
-external_ip='192.168.1.6'
-router_ip='192.168.2.1'
-internal_ip='192.168.2.0/24'
+ex_router='192.168.1.1'
+ex_ip_address='192.168.1.6'
+in_router='192.168.2.1'
+in_ip_address='192.168.2.0/24'
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
@@ -18,20 +18,20 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -P OUTPUT ACCEPT
 
 iptables -P FORWARD DROP
-iptables -A FORWARD -i wlan0 -o eth0 -s $internal_ip -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -s $in_ip_address -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # lo
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# ICMP internal_ip -> router_ip
-iptables -A INPUT -p icmp --icmp-type echo-request -s $internal_ip -d $router_ip -j ACCEPT
-iptables -A OUTPUT -p icmp --icmp-type echo-reply -s $router_ip -d $internal_ip -j ACCEPT
+# ICMP in_ip_address -> in_router
+iptables -A INPUT -p icmp --icmp-type echo-request -s $in_ip_address -d $in_router -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type echo-reply -s $in_router -d $in_ip_address -j ACCEPT
 
-# ICMP router_ip -> internal_ip
-iptables -A OUTPUT -p icmp --icmp-type echo-request -s $router_ip -d $internal_ip -j ACCEPT
-iptables -A INPUT -p icmp --icmp-type echo-reply -s $internal_ip -d $router_ip -j ACCEPT
+# ICMP in_router -> in_ip_address
+iptables -A OUTPUT -p icmp --icmp-type echo-request -s $in_router -d $in_ip_address -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-reply -s $in_ip_address -d $in_router -j ACCEPT
 
 # iTunes
 # UDP
@@ -44,39 +44,39 @@ iptables -A INPUT -p tcp --sport 5223 --dport 49152:65535 -j ACCEPT # Apple プ�
 
 # raspbian
 # DNS
-iptables -A OUTPUT -p udp -s $external_ip --sport 32768:60999 -d $gateway_ip --dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp -s $ex_ip_address --sport 32768:60999 -d $ex_router --dport 53 -j ACCEPT
 
 # NTP
-iptables -A OUTPUT -p udp -s $external_ip --sport 123 --dport 123 -j ACCEPT
+iptables -A OUTPUT -p udp -s $ex_ip_address --sport 123 --dport 123 -j ACCEPT
 
 # HTTPS
-iptables -A INPUT -p tcp --sport 443 -d $external_ip --dport 32768:60999 -j ACCEPT
-iptables -A OUTPUT -p tcp -s $external_ip --sport 32768:60999 --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --sport 443 -d $ex_ip_address --dport 32768:60999 -j ACCEPT
+iptables -A OUTPUT -p tcp -s $ex_ip_address --sport 32768:60999 --dport 443 -j ACCEPT
 
 # HTTP
-iptables -A INPUT -p tcp --sport 80 -d $external_ip --dport 32768:60999 -j ACCEPT
-iptables -A OUTPUT -p tcp -s $external_ip --sport 32768:60999 --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --sport 80 -d $ex_ip_address --dport 32768:60999 -j ACCEPT
+iptables -A OUTPUT -p tcp -s $ex_ip_address --sport 32768:60999 --dport 80 -j ACCEPT
 
 # mDNS
 # internal
-iptables -A INPUT -p udp -s $internal_ip --sport 5353 --dport 5353 -j ACCEPT
-iptables -A OUTPUT -p udp -s $router_ip --sport 5353 --dport 5353 -j ACCEPT
+iptables -A INPUT -p udp -s $in_ip_address --sport 5353 --dport 5353 -j ACCEPT
+iptables -A OUTPUT -p udp -s $in_router --sport 5353 --dport 5353 -j ACCEPT
 
 # external
-iptables -A INPUT -p udp -s $external_ip --sport 5353 --dport 5353 -j ACCEPT
-iptables -A OUTPUT -p udp -s $external_ip --sport 5353 --dport 5353 -j ACCEPT
+iptables -A INPUT -p udp -s $ex_ip_address --sport 5353 --dport 5353 -j ACCEPT
+iptables -A OUTPUT -p udp -s $ex_ip_address --sport 5353 --dport 5353 -j ACCEPT
 
 # DHCP
 # internal
-iptables -A INPUT -p udp -s $internal_ip --sport 68 -d $router_ip --dport 67 -j ACCEPT
-iptables -A OUTPUT -p udp -s $router_ip --sport 67 -d $internal_ip --dport 68 -j ACCEPT
+iptables -A INPUT -p udp -s $in_ip_address --sport 68 -d $in_router --dport 67 -j ACCEPT
+iptables -A OUTPUT -p udp -s $in_router --sport 67 -d $in_ip_address --dport 68 -j ACCEPT
 
 # external
-iptables -A INPUT -p udp -s $gateway_ip --sport 67 -d $external_ip --dport 68 -j ACCEPT
-iptables -A OUTPUT -p udp -s $external_ip --sport 68 -d $gateway_ip --dport 67 -j ACCEPT
+iptables -A INPUT -p udp -s $ex_router --sport 67 -d $ex_ip_address --dport 68 -j ACCEPT
+iptables -A OUTPUT -p udp -s $ex_ip_address --sport 68 -d $ex_router --dport 67 -j ACCEPT
 
 # nat
-iptables -t nat -A POSTROUTING -o eth0 -s $internal_ip -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -s $in_ip_address -j MASQUERADE
 
 # プライベートアドレス
 iptables -A OUTPUT -o eth0 -d 10.0.0.0/8 -j DROP
