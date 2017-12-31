d06f20857b8fd3fb78c1aa8fa2f7d04d0a4fb019
Jhonny Bravo <sanfranceshika5@gmail.com>
2017-12-31 18:58:51 +0900

    renamed: network/{get_sysctl_conf.rb, set_sysctl_conf.rb}   -> cookbooks/sysctl/{get_sysctl.conf, set_sysclt_conf.rb}

--- cookbooks/sysctl/get_sysctl_conf.rb
+++ cookbooks/sysctl/get_sysctl_conf.rb
@@ -1,15 +1,16 @@
 # get_sysctl_conf.rb
-# /etc/sysctl.conf -> ~/Templates/network/sysctl.conf.erb
+# /etc/sysctl.conf -> ~/Templates/sysctl/sysctl.conf.erb
 
 require 'dotenv'
 Dotenv.load
 
 USER_NAME=ENV["USER_NAME"]
 SRC="/etc/"
-DST="/home/#{USER_NAME}/Templates/network/"
+DST="/home/#{USER_NAME}/Templates/sysctl/"
 
 directory DST do
   action :create
+  user USER_NAME
   owner USER_NAME
   group USER_NAME
 end
@@ -17,6 +18,7 @@ end
 template "#{DST}sysctl.conf.erb" do
   action :create
   mode "644"
+  user USER_NAME
   owner USER_NAME
   group USER_NAME
   source "#{SRC}sysctl.conf"
--- cookbooks/sysctl/set_sysctl_conf.rb
+++ cookbooks/sysctl/set_sysctl_conf.rb
@@ -1,12 +1,12 @@
 # set_sysctl_conf.rb
-# ~/Templates/network/sysctl.conf.erb -> /etc/sysctl.conf
+# ~/Templates/sysctl/sysctl.conf.erb -> /etc/sysctl.conf
 
 require 'dotenv'
 Dotenv.load
 
 USER_NAME=ENV["USER_NAME"]
 DST="/etc/"
-SRC="/home/#{USER_NAME}/Templates/network/"
+SRC="/home/#{USER_NAME}/Templates/sysctl/"
 
 template "#{DST}sysctl.conf" do
   action :create
