1fe23b03942c56352493d2e9541b3eb39de9e6ba
Jhonny Bravo <sanfranceshika5@gmail.com>
2017-12-31 18:56:58 +0900

    renamed: network/{get_interfaces.rb, set_interfaces.rb}   -> cookbooks/interfaces/{get_interfaces.rb, set_interfaces.rb}

--- cookbooks/interfaces/get_interfaces.rb
+++ cookbooks/interfaces/get_interfaces.rb
@@ -1,15 +1,16 @@
 # get_interfaces.rb
-# /etc/network/interfaces -> ~/Templates/network/interfaces.erb
+# /etc/network/interfaces -> ~/Templates/interfaces/interfaces.erb
 
 require 'dotenv'
 Dotenv.load
 
 USER_NAME=ENV["USER_NAME"]
 SRC="/etc/network/"
-DST="/home/#{USER_NAME}/Templates/network/"
+DST="/home/#{USER_NAME}/Templates/interfaces/"
 
 directory DST do
   action :create
+  user USER_NAME
   owner USER_NAME
   group USER_NAME
 end
@@ -17,6 +18,7 @@ end
 template "#{DST}interfaces.erb" do
   action :create
   mode "644"
+  user USER_NAME
   owner USER_NAME
   group USER_NAME
   source "#{SRC}interfaces"
--- cookbooks/interfaces/set_interfaces.rb
+++ cookbooks/interfaces/set_interfaces.rb
@@ -1,12 +1,12 @@
 # set_interfaces.rb
-# ~/Templates/network/interfaces.erb -> /etc/network/interfaces
+# ~/Templates/interfaces/interfaces.erb -> /etc/network/interfaces
 
 require 'dotenv'
 Dotenv.load
 
 USER_NAME=ENV["USER_NAME"]
 DST="/etc/network/"
-SRC="/home/#{USER_NAME}/Templates/network/"
+SRC="/home/#{USER_NAME}/Templates/interfaces/"
 
 template "#{DST}interfaces" do
   action :create
